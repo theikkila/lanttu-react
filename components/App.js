3292e2e8073476e704df6e3e46b7faa8c1f7b1a7
Teemu Heikkilä <teemu.heikkila@reaktor.fi>
2015-05-27 00:23:13 +0300

    Store implemented

--- components/App.js
+++ components/App.js
@@ -1,14 +1,48 @@
 var React = require('react');
 
 var Hello = require('./Hello');
+var PageCalculator = require('./PageCalculator');
+var PageStore = require('../stores/PageStore');
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var cs = require('../dispatcher/constants');
+
+
+function getPagesState () {
+    return {
+        allPages: PageStore.getAll()
+    }    
+}
 
 var App = React.createClass({
+    getInitialState() {
+        return getPagesState();
+    },
+	componentDidMount() {
+        PageStore.addChangeListener(this.handleChange);
+    },
+    componentWillUnmount() {  
+        PageStore.removeChangeListener(this.handleChange);
+	},
+	handleChange(pages){
+		this.setState(getPagesState());
+	},
+	createNewItem() {
+		AppDispatcher.dispatch({
+        	type: cs.CREATE,
+        	data: { slug: "testi", name: 'Marco', content: "adasdasdadasdasd" } // example data
+    	});
+	},
     render(){
-
+    	var rpages = this.state.allPages.map(function (page) {
+    		return <li>{page.name} <pre>{page.slug}</pre></li>
+    	});
         return (
 
             <div>
+            	<button onClick={ this.createNewItem } >Paina</button><PageCalculator />
                 <Hello name="Matias" />
+                { rpages }
+                
             </div>
             )
     }
