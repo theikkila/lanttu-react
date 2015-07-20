c0bccc98cbcf0a3e914ed8bf641078b84a9a96ff
Teemu Heikkilä <sienikasvusto@pistoke.org>
2015-07-20 18:13:24 +0300

    Revert "Add twitter bootstrap"

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,13 @@
 var React  = require('react');
+var Navigation = require('./navigation/Navigation');
 
 var Header = React.createClass({
 
   render: function(){
     return(
-      <div id="header" className="col-xs-12">
-        <h1>Header</h1>
+      <div id="header">
+      <h1>Header</h1>
+        <Navigation />
       </div>
     );
   }
