a41c8eddeb10dfbc7737fb7496f8d650ab3a83a4
Johannes Elmnäinen <sanhologin@gmail.com>
2015-08-10 19:18:08 +0300

    add logo

--- src/dispatcher/constants.js
+++ src/dispatcher/constants.js
@@ -1,7 +1,8 @@
 module.exports = {
-	CHANGE: 'CHANGE',
-	CREATEPOST: 'CREATEPOST',
-	CREATEPAGE: 'CREATEPAGE',
-	DESTROY: 'DESTROY',
-	JOKELAN_JSON_API: 'http://s15.joke-lan.net/api/v0'
+	CHANGE: 			'CHANGE',
+	CREATEPOST: 		'CREATEPOST',
+	CREATEPAGE: 		'CREATEPAGE',
+	DESTROY: 			'DESTROY',
+	JOKELAN_JSON_API: 	'http://s15.joke-lan.net/api/v0',
+	BASE_URL: 			'http://s15.joke-lan.net'
 };
