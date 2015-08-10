870f13d92d82599325980cd56ba734633018797c
Teemu Heikkilä <teemu.heikkila@reaktor.fi>
2015-08-10 21:49:54 +0300

    Responsive navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -7,6 +7,7 @@ var cs = require('../../dispatcher/constants');
 var stateGet = function () {
   return {
             nav: AppState.navigation.toJSON(),
+            navshow: false,
             router: AppState.router.toJSON()
            };
 }
@@ -23,10 +24,16 @@ var Navigation = React.createClass({
   handleChange() {
     this.setState(stateGet());
   },
+  handleNavToggle() {
+    var state = this.state;
+    state.navshow = !state.navshow;
+    console.log("moi", state);
+    this.setState(state);
+  },
   render: function(){
     var self = this;
     var menuItems = this.state.nav.map(function(item){
-      var selected_class = item.object_id == self.state.router.navid ? 'selected' : 'unselected';
+      var selected_class = item.name == self.state.router.navid ? 'selected' : 'unselected';
       return(
         <li key={item.ID} className={selected_class}>
           <a href={"#/" + item.type + "/" + item.name} key={item.id}>
@@ -36,11 +43,23 @@ var Navigation = React.createClass({
       );
     });
 
+    var nav_classes = "main-nav col-xs-12 col-md-3";
+    if (!this.state.navshow) {
+      nav_classes += " menu-hidden";
+    }
+
     return(
-      <nav className="main-nav col-xs-12 col-md-3">
+      <nav className={nav_classes}>
         <ul>
           {menuItems}
         </ul>
+        <button className="menu-btn" onClick={this.handleNavToggle}>
+          <img src="../src/style/images/joke-lan-logo-small.png" className="nav-logo"/>
+          <div className="right">
+          Valikko
+          <div className="mask-icon-white"></div>
+          </div>
+        </button>
       </nav>
     );
   }
