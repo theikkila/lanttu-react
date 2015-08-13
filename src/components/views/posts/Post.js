68eb2e311ef8d1908c59b597d367cb414feb0ea7
Teemu Heikkilä <teemu.heikkila@reaktor.fi>
2015-08-14 00:42:38 +0300

    Fixed li spaces

--- src/components/views/posts/Post.js
+++ src/components/views/posts/Post.js
@@ -3,11 +3,15 @@ var React  = require('react');
 var Post = React.createClass({
 	render: function(){
 		var post = this.props.post;
-		var content_with_br = post.content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+		var content_with_br = post.content.split(/\r\n|\n|\r/).map(function (row) {
+			var trimmed = row.trim();
+			if (trimmed[trimmed.length-1] != '>') { row = row + "<br>";}
+			return row;
+		}).join('');
 		return  <div className="post" key={post.ID}>
-				 <h1>{post.title}</h1>
-				 <div dangerouslySetInnerHTML={{__html: content_with_br}}></div>
-			    </div>
+		<h1>{post.title}</h1>
+		<div dangerouslySetInnerHTML={{__html: content_with_br}}></div>
+		</div>
 	}
 });
 
